48435ba042cbfcb7a701c372dabdb67c72ac3a94
Jim Clay <james.o.clay@gmail.com>
2018-10-05 20:27:17 -0400

    Added a transpose test.

--- test/MatrixTest.cpp
+++ test/MatrixTest.cpp
@@ -174,6 +174,56 @@ TEST(Matrix2d_Methods, Reshape) {
 	EXPECT_EQ(12, mat1(3, 2));
 }
 
+TEST(Matrix2d_Methods, Transpose) {
+	MatrixDSP::Matrix2d<float> mat1({ {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} });
+	MatrixDSP::Matrix2d<float> mat2(1, 1);
+
+	//Timer timer;
+	//timer.startTimer();
+	//for (int index = 0; index < 1000001; index++)
+	{
+		transpose(mat1, mat2);
+	}
+	//double timeDiff = timer.getTimeDiff();
+	//printf("Transpose time = %g s\n", timeDiff);
+	EXPECT_EQ(4, mat2.getRows());
+	EXPECT_EQ(3, mat2.getCols());
+	EXPECT_EQ(1, mat2(0, 0));
+	EXPECT_EQ(2, mat2(1, 0));
+	EXPECT_EQ(3, mat2(2, 0));
+	EXPECT_EQ(4, mat2(3, 0));
+	EXPECT_EQ(5, mat2(0, 1));
+	EXPECT_EQ(6, mat2(1, 1));
+	EXPECT_EQ(7, mat2(2, 1));
+	EXPECT_EQ(8, mat2(3, 1));
+	EXPECT_EQ(9, mat2(0, 2));
+	EXPECT_EQ(10, mat2(1, 2));
+	EXPECT_EQ(11, mat2(2, 2));
+	EXPECT_EQ(12, mat2(3, 2));
+
+	//timer.startTimer();
+	//for (int index = 0; index < 1000001; index++)
+	{
+		transpose(mat1);
+	}
+	//timeDiff = timer.getTimeDiff();
+	//printf("Transpose time = %g s\n", timeDiff);
+	EXPECT_EQ(4, mat1.getRows());
+	EXPECT_EQ(3, mat1.getCols());
+	EXPECT_EQ(1, mat1(0, 0));
+	EXPECT_EQ(2, mat1(1, 0));
+	EXPECT_EQ(3, mat1(2, 0));
+	EXPECT_EQ(4, mat1(3, 0));
+	EXPECT_EQ(5, mat1(0, 1));
+	EXPECT_EQ(6, mat1(1, 1));
+	EXPECT_EQ(7, mat1(2, 1));
+	EXPECT_EQ(8, mat1(3, 1));
+	EXPECT_EQ(9, mat1(0, 2));
+	EXPECT_EQ(10, mat1(1, 2));
+	EXPECT_EQ(11, mat1(2, 2));
+	EXPECT_EQ(12, mat1(3, 2));
+}
+
 TEST(Matrix2dIterator, PlusPlus) {
 	MatrixDSP::Matrix2d<float> mat({ {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} });
 	float horizontalResults[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
